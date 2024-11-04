558408717b5910174bfaf0303a850393c9bac60b
Otto Haarahiltunen <ottoperttulasta@gmail.com>
2024-11-04 10:37:58 +0200

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coveragePathIgnorePatterns: [
-      '/node_modules/',
-      '/.internal/'  // Exclude .internal directory
-    ]
-  };
